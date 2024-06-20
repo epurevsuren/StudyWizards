274dadeb7643032253ed32c56a43ce8cff34b1eb
Purevsuren Erdene (Peter) <purevsuren.er@gmail.com>
2024-06-20 10:36:06 +1000

    Initial Commit

---
+++ StudyWizards/StudyWizardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWizardsApp.swift
+//  StudyWizards
+//
+//  Created by Purevsuren Erdene on 20/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct StudyWizardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
